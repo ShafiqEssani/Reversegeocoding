5c2e3f795725862ced3d133309241cc3119eae4f
ShafiqEssani <shafiqshamsessani@gmail.com>
2018-03-13 13:30:29 +0500

    Final Comit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NativeGeocoder, NativeGeocoderReverseResult } from '@ionic-native/native-geocoder';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,17 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  output: any;
+  constructor(private nativeGeocoder: NativeGeocoder) {
+  }
 
+  ionViewWillLoad() {
+    this.nativeGeocoder.reverseGeocode(24.874324, 67.067461)
+      .then((result: NativeGeocoderReverseResult) => {
+        console.log((result))
+        this.output = result[0];
+      })
+      .catch((error: any) => console.log(error));
   }
 
 }
